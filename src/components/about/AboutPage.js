316539dc8cf99a38cbc8257e7d8896b98d3970ea
e.georgantzos <e.georgantzos@exus.co.uk>
2018-03-02 16:56:32 +0200

    redux folder structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>About React, Redux and React Router in ES6 among others</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
